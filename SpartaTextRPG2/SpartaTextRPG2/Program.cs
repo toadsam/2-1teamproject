68c4fd0bae513c215fd7a69271b8d89452e45b5b
LeeMinyeol <ingyeol16@gmail.com>
2023-08-28 11:44:13 +0900

    초기 코드
    초기 코드

---
+++ SpartaTextRPG2/SpartaTextRPG2/Program.cs
@@ -0,0 +1,271 @@
+﻿namespace SpartaTextRPG2
+{
+    internal class Program
+    {
+        private static Character player;
+        private static Monster[] monsters; // 몬스터 종류
+
+
+        static void Main(string[] args)
+        {
+            GameDataSetting();
+            DisplayGameIntro();
+        }
+
+        static void GameDataSetting()
+        {
+            // 캐릭터 정보 세팅
+            player = new Character("민열", "전사", 1, 10, 5, 100, 15000, 100, 100);
+            monsters = new Monster[5]; // 마릿수는
+            // 아이템 정보 세팅 => 상점에서 골드 대신 판매 완료를 하려면 어떻게 할까
+
+
+            Monster monster1 = new Monster("미니언", 2, 15, 15,  false);
+            Monster monster2 = new Monster("대포미니언", 3, 25, 25, false);
+            Monster monster3 = new Monster("공허충", 5, 10, 10, false);
+            monsters = new Monster[3] { monster1, monster2, monster3 };
+
+        }
+
+        static void DisplayGameIntro()
+        {
+            Console.Clear();
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 전투시작");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+            int input = CheckValidInput(1, 2);
+            switch (input)
+            {
+                case 1:
+                    DisplayMyInfo();
+                    break;
+
+                case 2:
+                    DisplayBattle();
+                    break;
+            }
+        }
+
+        static void DisplayMyInfo()
+        {
+            Console.Clear();
+
+            Console.WriteLine("상태보기");
+            Console.WriteLine("캐릭터의 정보를 표시합니다.");
+            Console.WriteLine();
+            Console.WriteLine($"Lv.{player.Level}");
+            Console.WriteLine($"{player.Name}({player.Job})");
+            Console.WriteLine($"공격력 :{player.Atk}");
+            Console.WriteLine($"방어력 : {player.Def}");
+            Console.WriteLine($"체력 : {player.Hp}");
+            Console.WriteLine($"Gold : {player.Gold} G");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+            int input = CheckValidInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+            }
+        }
+  
+
+        static void DisplayBattle()
+        {
+
+            // 공격할 몬스터 번호를 선택
+            // 
+            Console.Clear();
+            Console.WriteLine("Battle!!");
+            Console.WriteLine();
+
+            Console.WriteLine("[몬스터 종류]");
+
+            for (int i = 0; i < monsters.Length; i++)
+            {
+                Console.Write($"- {i + 1} ");
+                Console.Write($"Lv .{monsters[i].Level} {monsters[i].Name} HP {monsters[i].CorHealth} \n");
+                Console.WriteLine();
+
+            }
+        
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("[내 정보]");
+            Console.WriteLine($"Lv. {player.Level} {player.Name} ({player.Job})");
+            Console.WriteLine($"HP {player.CorHealth} / {player.MaxHealth}");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("1. 공격");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요");
+            
+            int input = CheckValidInput(0, 1);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+
+                default:
+                    
+                    DisplayBattleInfo();
+                    break;
+            }
+        }
+        static void DisplayEnd()
+        {
+
+        }
+
+        static void DisplayBattleInfo()
+        {
+
+            // 공격할 몬스터 번호를 선택
+            // 
+            Console.Clear();
+            Console.WriteLine("Battle!!");
+            Console.WriteLine();
+
+            Console.WriteLine("[몬스터 종류]");
+
+            for (int i = 0; i < monsters.Length; i++)
+            {
+                Console.Write($"- {i + 1} ");
+                Console.Write($"Lv .{monsters[i].Level} {monsters[i].Name} HP {monsters[i].CorHealth} \n");
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("[내 정보]");
+            Console.WriteLine($"Lv. {player.Level} {player.Name} ({player.Job})");
+            Console.WriteLine($"HP {player.CorHealth} / {player.MaxHealth}");
+            Console.WriteLine();
+            Console.WriteLine("0. 취소");
+            Console.WriteLine();
+            Console.WriteLine("대상을 선택해주세요");
+
+            int input = CheckValidInput(0, monsters.Length);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+
+                default:
+
+                    DisplayAttack();
+                    break;
+            }
+        }
+        /*static void DisplayEnd()
+        {
+
+        }*/
+        static void DisplayAttack()
+        {
+            // 공격 기능
+            // 공격 결과표시 화면
+            // 공격 대상 판별
+            // thread.sleep or 0
+        }
+
+
+        static int CheckValidInput(int min, int max)    //사용자로부터 입력받고 입력받은 값이 매개변수 사이의 값이 아니면 다시 입력받음
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
+    }
+    
+
+    public class Character  // 캐릭터 정보를 담당하는 클래스
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Level { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Hp { get; set; }
+        public float Gold { get; set; }
+        public int CorHealth { get; set; }
+        public int MaxHealth { get; set; }
+
+        public Character(string name, string job, int level, int atk, int def, int hp, float gold, int corHealth, int maxHealth)
+        {
+            Name = name;
+            Job = job;
+            Level = level;
+            Atk = atk;
+            Def = def;
+            Hp = hp;
+            Gold = gold;
+            CorHealth = corHealth;
+            MaxHealth = maxHealth;
+        }
+    }
+    
+
+    public class Item   //아이템 정보를 담당하는 클래스
+    {
+        public string Name { get; set; }
+        public int Atk { get; } // 필요 없음
+        public int Def { get; } // 필요 없음
+        public string Inform { get; } // 필요 없음
+        public bool IsEquip { get; set; } // 필요 없음
+        public string Type { get; } // 필요 없음
+        public float Gold { get; } // 필요 없음
+        public bool IsBuy { get; set; } // 필요 없음
+
+        public Item(string name, int atk, int def, string inform, bool isEquip, string type, float gold, bool isBuy)
+        {
+            Name = name;
+            Atk = atk;
+            Def = def;
+            Inform = inform;
+            IsEquip = isEquip;
+            Type = type;
+            Gold = gold;
+            IsBuy = isBuy;
+
+        }
+
+    }
+
+    public class Monster
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int MaxHealth { get; set; }
+        public int CorHealth { get; set; }
+        public bool IsDead { get; set; }
+
+        public Monster(string name, int level, int maxHealth, int corHealth, bool isDead)
+        {
+            Name = name;
+            Level = level;
+            MaxHealth = maxHealth;
+            CorHealth = corHealth;
+            isDead = isDead;
+        }
+    }
+}
