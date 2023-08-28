18e79918ad214fe1b39829b81b14fa00ef6bd03f
LeeMinyeol <ingyeol16@gmail.com>
2023-08-28 12:44:13 +0900

    공격페이지

--- SpartaTextRPG2/SpartaTextRPG2/Program.cs
+++ SpartaTextRPG2/SpartaTextRPG2/Program.cs
@@ -10,19 +10,19 @@
         {
             GameDataSetting();
             DisplayGameIntro();
-        0}
+        }
 
         static void GameDataSetting()
         {
             // 캐릭터 정보 세팅
-            player = new Character("민열", "전사", 1, 10, 5, 100, 15000, 100, 100);
+            player = new Character("민열", "전사", 1, 10, 5, 100, 15000, 100, 100, true);
             monsters = new Monster[5]; // 마릿수는
             // 아이템 정보 세팅 => 상점에서 골드 대신 판매 완료를 하려면 어떻게 할까
 
 
-            Monster monster1 = new Monster("미니언", 2, 15, 15,  false);
-            Monster monster2 = new Monster("대포미니언", 3, 25, 25, false);
-            Monster monster3 = new Monster("공허충", 5, 10, 10, false);
+            Monster monster1 = new Monster("미니언", 2, 15, 15, 5, false);
+            Monster monster2 = new Monster("대포미니언", 3, 25, 25, 10, false);
+            Monster monster3 = new Monster("공허충", 5, 10, 10, 15, false);
             monsters = new Monster[3] { monster1, monster2, monster3 };
 
         }
@@ -31,8 +31,8 @@
         {
             Console.Clear();
 
-            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-            Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("1. 상태보기");
             Console.WriteLine("2. 전투시작");
@@ -76,7 +76,7 @@
                     break;
             }
         }
-  
+
 
         static void DisplayBattle()
         {
@@ -91,23 +91,21 @@
 
             for (int i = 0; i < monsters.Length; i++)
             {
-                Console.Write($"- {i + 1} ");
-                Console.Write($"Lv .{monsters[i].Level} {monsters[i].Name} HP {monsters[i].CorHealth} \n");
-                Console.WriteLine();
+                Console.Write($"Lv.{monsters[i].Level} {monsters[i].Name} HP {monsters[i].CurHealth} \n");
 
             }
-        
+
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("[내 정보]");
-            Console.WriteLine($"Lv. {player.Level} {player.Name} ({player.Job})");
-            Console.WriteLine($"HP {player.CorHealth} / {player.MaxHealth}");
+            Console.WriteLine($"Lv.{player.Level} {player.Name} ({player.Job})");
+            Console.WriteLine($"HP {player.CurHealth} / {player.MaxHealth}");
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             Console.WriteLine("1. 공격");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요");
-            
+
             int input = CheckValidInput(0, 1);
             switch (input)
             {
@@ -116,7 +114,7 @@
                     break;
 
                 default:
-                    
+
                     DisplayBattleInfo();
                     break;
             }
@@ -139,15 +137,24 @@
 
             for (int i = 0; i < monsters.Length; i++)
             {
-                Console.Write($"- {i + 1} ");
-                Console.Write($"Lv .{monsters[i].Level} {monsters[i].Name} HP {monsters[i].CorHealth} \n");
-                Console.WriteLine();
+                Console.Write($"{i + 1} ");
+                if (monsters[i].IsDead == true)
+                {
+                    Console.Write($"Lv.{monsters[i].Level} {monsters[i].Name} Dead"); //몬스터 체력 0 이하면 Dead 출력
+
+                }
+                else
+                {
+                    Console.Write($"Lv.{monsters[i].Level} {monsters[i].Name} HP {monsters[i].CurHealth} \n");
+
+                }
+
 
             }
             Console.WriteLine();
             Console.WriteLine("[내 정보]");
-            Console.WriteLine($"Lv. {player.Level} {player.Name} ({player.Job})");
-            Console.WriteLine($"HP {player.CorHealth} / {player.MaxHealth}");
+            Console.WriteLine($"Lv.{player.Level} {player.Name} ({player.Job})");
+            Console.WriteLine($"HP {player.CurHealth} / {player.MaxHealth}");
             Console.WriteLine();
             Console.WriteLine("0. 취소");
             Console.WriteLine();
@@ -157,12 +164,12 @@
             switch (input)
             {
                 case 0:
-                    DisplayGameIntro();
+                    DisplayBattle();
                     break;
 
                 default:
 
-                    DisplayAttack();
+                    DisplayAttack(input);
                     break;
             }
         }
@@ -170,12 +177,83 @@
         {
 
         }*/
-        static void DisplayAttack()
+        static void DisplayAttack(int inp)
         {
-            // 공격 기능
+            // 공격 기능 
             // 공격 결과표시 화면
             // 공격 대상 판별
-            // thread.sleep or 0
+            Console.Clear();
+            Console.WriteLine("Battle!!");
+            Console.WriteLine();
+            if (player.MyTurn == true)
+            {
+                Console.WriteLine($"{player.Name}의 공격!");
+                Console.WriteLine($"{monsters[inp - 1].Name}을(를) 맞췄습니다. [데미지 : {player.Atk}]"); // 데미지는 나중에 랜덤 넣을 예정
+                Console.WriteLine();
+                Console.WriteLine($"{monsters[inp - 1].Name}");
+
+                monsters[inp - 1].CurHealth -= player.Atk;
+
+                if (monsters[inp - 1].CurHealth <= 0)
+                {
+                    Console.WriteLine($"HP {monsters[inp - 1].CurHealth} -> Dead");
+                    monsters[inp - 1].CurHealth = 0;
+                    monsters[inp - 1].IsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine($"HP {monsters[inp - 1].CurHealth} -> {monsters[inp - 1].CurHealth - player.Atk}");
+
+                }
+                player.MyTurn = false;
+                Console.WriteLine("0. 다음"); // 0으로 넘어갈지 thread.sleep으로 시간차 두고 넘어갈지 정하기
+            }
+            else
+            {
+
+                foreach (Monster mon in monsters) // 살아있는 몬스터 전부 돌아가면서 공격
+                {
+                    if (mon.IsDead == false)
+                    {
+                        Console.WriteLine($"Lv.{mon.Level} {mon.Name}의 공격!");
+                        Console.WriteLine($"{player.Name}을(를) 맞췄습니다. [데미지 : {mon.Atk}]");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine($"Lv.{player.Level} {player.Name}");
+                        Console.WriteLine($"HP {player.CurHealth} {player.CurHealth - mon.Atk}");
+                        player.CurHealth -= mon.Atk;
+                        Console.WriteLine();
+
+                    }
+                    else
+                    {
+                        // 몬스터가 다 죽었을 때
+                    }
+                    if (player.CurHealth <= 0)
+                    {
+                        // DisplayGameOver(); 플레이어 체력이 0 이하면 게임종료 실행
+                        break;
+                    }
+
+                }
+                player.MyTurn = true;
+                Console.WriteLine("0. 다음"); // 0으로 넘어갈지 thread.sleep으로 시간차 두고 넘어갈지 정하기
+            }
+
+
+
+            int input = CheckValidInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    DisplayBattleInfo();
+                    break;
+
+                default:
+
+                    DisplayAttack(input);
+                    break;
+            }
         }
 
 
@@ -195,81 +273,8 @@
                 Console.WriteLine("잘못된 입력입니다.");
             }
         }
-
-
-        // 윤경: 전투 결과
-        static void DisplayGameOver()
-        {
-            DisplayVictory();
-
-
-            if (player.CorHealth > 0 && player.CorHealth <= player.MaxHealth)
-            {
-                DisplayVictory();
-            }
-            else if (player.CorHealth == 0)
-            {
-                DisplayLose();
-            }
-            
-        }
-
-        private static void DisplayVictory()
-        {
-            Console.Clear();
-
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("Battle!! - Result");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Victory");
-            Console.ResetColor();
-
-            Console.WriteLine();
-            Console.WriteLine("던전에서 몬스터 3마리를 잡았습니다.");
-
-            Console.WriteLine();
-            Console.WriteLine($"Lv.{player.Level} {player.Name}");
-            Console.WriteLine($"HP {player.MaxHealth} -> {player.CorHealth} ");
-
-            Console.WriteLine();
-            Console.WriteLine("0. 다음");
-
-            int input = CheckValidInput(0, 0);
-            switch (input)
-            {
-                case 0:
-                    DisplayGameIntro();
-                    break;
-            }
-        }
-
-        private static void DisplayLose()
-        {
-            Console.Clear();
-
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("Battle!! - Result");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("You Lose");
-            Console.ResetColor();
-
-            Console.WriteLine();
-            Console.WriteLine($"Lv.{player.Level} {player.Name}");
-            Console.WriteLine($"HP {player.MaxHealth} -> {player.CorHealth} ");
-
-            Console.WriteLine();
-            Console.WriteLine("0. 다음");
-
-            int input = CheckValidInput(0, 0);
-            switch (input)
-            {
-                case 0:
-                    DisplayGameIntro();
-                    break;
-            }
-        }
     }
-    
+
 
     public class Character  // 캐릭터 정보를 담당하는 클래스
     {
@@ -280,10 +285,12 @@
         public int Def { get; set; }
         public int Hp { get; set; }
         public float Gold { get; set; }
-        public int CorHealth { get; set; }
+        public int CurHealth { get; set; }
         public int MaxHealth { get; set; }
 
-        public Character(string name, string job, int level, int atk, int def, int hp, float gold, int corHealth, int maxHealth)
+        public bool MyTurn { get; set; }
+
+        public Character(string name, string job, int level, int atk, int def, int hp, float gold, int curHealth, int maxHealth, bool myTurn)
         {
             Name = name;
             Job = job;
@@ -292,36 +299,10 @@
             Def = def;
             Hp = hp;
             Gold = gold;
-            CorHealth = corHealth;
+            CurHealth = curHealth;
             MaxHealth = maxHealth;
+            MyTurn = myTurn;
         }
-    }
-    
-
-    public class Item   //아이템 정보를 담당하는 클래스
-    {
-        public string Name { get; set; }
-        public int Atk { get; } // 필요 없음
-        public int Def { get; } // 필요 없음
-        public string Inform { get; } // 필요 없음
-        public bool IsEquip { get; set; } // 필요 없음
-        public string Type { get; } // 필요 없음
-        public float Gold { get; } // 필요 없음
-        public bool IsBuy { get; set; } // 필요 없음
-
-        public Item(string name, int atk, int def, string inform, bool isEquip, string type, float gold, bool isBuy)
-        {
-            Name = name;
-            Atk = atk;
-            Def = def;
-            Inform = inform;
-            IsEquip = isEquip;
-            Type = type;
-            Gold = gold;
-            IsBuy = isBuy;
-
-        }
-
     }
 
     public class Monster
@@ -329,15 +310,21 @@
         public string Name { get; set; }
         public int Level { get; set; }
         public int MaxHealth { get; set; }
-        public int CorHealth { get; set; }
+        public int CurHealth { get; set; }
+        public int Atk { get; set; }
+
         public bool IsDead { get; set; }
 
-        public Monster(string name, int level, int maxHealth, int corHealth, bool isDead)
+
+
+
+        public Monster(string name, int level, int maxHealth, int curHealth, int atk, bool isDead)
         {
             Name = name;
             Level = level;
             MaxHealth = maxHealth;
-            CorHealth = corHealth;
+            CurHealth = curHealth;
+            Atk = atk;
             isDead = isDead;
         }
     }
